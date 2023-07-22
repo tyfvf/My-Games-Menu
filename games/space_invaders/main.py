1a26a32c2078b331b88f06e1c996ecc3dbd979fb
Lucas Brizola <tyfvf2@gmail.com>
2023-07-21 22:22:15 -0300

    Basic setup

---
+++ games/space_invaders/main.py
@@ -0,0 +1,32 @@
+import pygame, sys
+
+class Game:
+    def __init__(self):
+        pass
+
+
+    def run(self):
+        pass
+        # update all sprite groups
+        # draw all spire groups
+
+
+if __name__ == '__main__':
+    pygame.init()
+    screen_width = 600
+    screen_height = 600
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    clock = pygame.time.Clock()
+    game = Game()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        screen.fill((30, 30, 30))
+        game.run()
+
+        pygame.display.flip()
+        clock.tick(60)
